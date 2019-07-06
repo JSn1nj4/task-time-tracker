c3486e041c25dca8e6ba8abc2218f0db4b901756
Elliot Derhay <e.j.derhay@gmail.com>
2019-07-05 22:59:00 -0400

    Begin working on task state management
    This should work in theory. Will need to do an actual test.

---
+++ store/tasks.js
@@ -0,0 +1,24 @@
+export const state = () => ({
+  list: [],
+})
+
+export const mutations = {
+  add(state, desc) {
+    state.list.push({
+      desc: desc,
+      start: null,
+      end: null,
+      accumulated: 0,
+    })
+  },
+  delete(state, { task }) {
+    state.list.splice(state.list.indexOf(task), 1)
+  },
+  setStart(state, task) {
+    task.start = new Date()
+  },
+  setEnd(state, task) {
+    task.end = new Date()
+    task.accumulated = task.end - task.start
+  }
+}
